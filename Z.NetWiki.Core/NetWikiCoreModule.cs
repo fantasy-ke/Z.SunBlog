300f974308cff1caca9581feb91a38db1e53519b
fantasy-ke <2246080525@qq.com>
2023-04-25 23:12:34 +0800

    项目依赖

--- Z.NetWiki.Core/NetWikiCoreModule.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Z.NetWiki.Core
-{
-    public class NetWikiCoreModule
-    {
-
-    }
-}
---
+++ Z.NetWiki.Core/NetWikiDomainModule.cs
@@ -0,0 +1,7 @@
+﻿namespace Z.NetWiki.Domain
+{
+    public class NetWikiDomainModule
+    {
+
+    }
+}
