09ecae0a96c3784b4fc5694ecc780bc85ba210c8
fantasy_ke <2246080525@qq.com>
2023-07-14 18:02:15 +0800

    新增manager

---
+++ src/Z.Ddd.Domain/Authorization/Authorize/AuthorizeManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z.Module.DependencyInjection;
+
+namespace Z.Ddd.Domain.Authorization.Authorize
+{
+
+    public class AuthorizeManager : IAuthorizeManager
+    {
+        public async Task AddAuthorizeRegiester()
+        {
+           await Task.FromResult(0);
+        }
+    }
+
+    public interface IAuthorizeManager:ITransientDependency
+    {
+        Task AddAuthorizeRegiester();
+    }
+}
