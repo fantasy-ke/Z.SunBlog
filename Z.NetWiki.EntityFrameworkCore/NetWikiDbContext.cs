0bf9320719daa1588ae23a0452e18a739a92599e
fantasy_ke <2246080525@qq.com>
2023-08-04 18:03:32 +0800

    提交

---
+++ Z.NetWiki.EntityFrameworkCore/NetWikiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z.EntityFrameworkCore;
+
+namespace Z.NetWiki.EntityFrameworkCore
+{
+    public class NetWikiDbContext : ZDbContext<NetWikiDbContext>
+    {
+        public NetWikiDbContext(DbContextOptions<NetWikiDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
