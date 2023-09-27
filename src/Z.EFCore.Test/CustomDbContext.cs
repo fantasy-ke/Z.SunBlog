d4f150e0e6e2f4df34ff22013eb7e3416459f23c
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-09-27 18:15:37 +0800

    单元测试

---
+++ src/Z.EFCore.Test/CustomDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z.EntityFrameworkCore;
+using Z.EntityFrameworkCore.Attributes;
+using Z.Module.DependencyInjection;
+
+namespace Z.EFCore.Test;
+
+[ConnectionStringName("App:ConnectionString:Default")]
+public class CustomDbContext : ZDbContext<CustomDbContext>, IScopedDependency
+{
+    public CustomDbContext(DbContextOptions<CustomDbContext> options) : base(options)
+    {
+    }
+}
