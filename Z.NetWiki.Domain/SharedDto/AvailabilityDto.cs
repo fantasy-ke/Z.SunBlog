51a81b5ca2dd36683763edb7fbdb8d0670cdc4a8
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-12 14:21:31 +0800

    基础dto

---
+++ Z.NetWiki.Domain/SharedDto/AvailabilityDto.cs
@@ -0,0 +1,11 @@
+﻿using Z.NetWiki.Domain.Enum;
+
+namespace Z.NetWiki.Domain.SharedDto;
+
+public class AvailabilityDto : KeyDto
+{
+    /// <summary>
+    /// 状态
+    /// </summary>
+    public AvailabilityStatus Status { get; set; }
+}
---
+++ Z.NetWiki.Domain/SharedDto/SelectOutput.cs
@@ -0,0 +1,16 @@
+﻿namespace Z.NetWiki.Domain.SharedDto;
+/// <summary>
+/// 下拉框
+/// </summary>
+public class SelectOutput
+{
+    /// <summary>
+    /// 文本
+    /// </summary>
+    public string Label { get; set; }
+
+    /// <summary>
+    /// 值
+    /// </summary>
+    public long Value { get; set; }
+}
