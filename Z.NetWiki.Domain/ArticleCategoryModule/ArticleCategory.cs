28759489df1a7bfa38eb15956d5bfbd16aee025d
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-12 14:22:17 +0800

    文章栏目Module

---
+++ Z.NetWiki.Domain/ArticleCategoryModule/ArticleCategory.cs
@@ -0,0 +1,18 @@
+﻿using Z.Ddd.Common.Entities.Auditing;
+
+namespace Z.NetWiki.Domain.ArticleCategoryModule;
+/// <summary>
+/// 文章所属栏目表
+/// </summary>
+public class ArticleCategory : FullAuditedEntity<Guid>
+{
+    /// <summary>
+    /// 文章Id
+    /// </summary>
+    public Guid ArticleId { get; set; }
+
+    /// <summary>
+    /// 栏目Id
+    /// </summary>
+    public Guid CategoryId { get; set; }
+}
