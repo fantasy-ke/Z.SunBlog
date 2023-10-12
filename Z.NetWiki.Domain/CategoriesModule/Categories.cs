1e3cff43a8db4b1a98527a035e9c3dc27b0aa559
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-12 14:22:52 +0800

    栏目Module

---
+++ Z.NetWiki.Domain/CategoriesModule/Categories.cs
@@ -0,0 +1,67 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Z.Ddd.Common.Entities.Auditing;
+using Z.NetWiki.Domain.Enum;
+
+namespace Z.NetWiki.Domain.CategoriesModule;
+
+/// <summary>
+/// 文章栏目表
+/// </summary>
+public class Categories : FullAuditedEntity<Guid>
+{
+    /// <summary>
+    /// 栏目名称
+    /// </summary>
+    [MaxLength(32)]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 父级id
+    /// </summary>
+    public Guid? ParentId { get; set; }
+
+    /// <summary>
+    /// 封面图
+    /// </summary>
+    [MaxLength(256)]
+    public string Cover { get; set; }
+
+    /// <summary>
+    /// 可用状态
+    /// </summary>
+    public AvailabilityStatus Status { get; set; }
+
+    /// <summary>
+    /// 排序值（值越小越靠前）
+    /// </summary>
+    public int Sort { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    [MaxLength(256)]
+    public string? Remark { get; set; }
+
+    /// <summary>
+    /// 创建人
+    /// </summary>
+    public long CreatedUserId { get; set; }
+
+    /// <summary>
+    /// 标记删除
+    /// </summary>
+    public bool DeleteMark { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    public DateTime CreatedTime { get; set; }
+
+    /// <summary>
+    /// 子栏目
+    /// </summary>
+    [NotMapped]
+    public List<Categories> Children { get; set; } = new();
+}
