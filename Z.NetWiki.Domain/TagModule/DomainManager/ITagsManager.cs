b6dbaaebe5c26f8137e2c0797a3d5dd6c02f69af
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-12 14:49:35 +0800

    文章标签领域层

---
+++ Z.NetWiki.Domain/TagModule/DomainManager/ITagsManager.cs
@@ -0,0 +1,9 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+using Z.NetWiki.Domain.TagModule;
+
+namespace Z.NetWiki.Domain.TagsModule.DomainManager
+{
+    public interface ITagsManager : IBusinessDomainService<Tags>
+    {
+    }
+}
