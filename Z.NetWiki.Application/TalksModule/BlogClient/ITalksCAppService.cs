01d9ee2c271fb5ea8b4c11521b4111418dc5bf69
Fantasy-Ke <2246080525@qq.com>
2023-10-13 21:49:21 +0800

    说说

---
+++ Z.NetWiki.Application/TalksModule/BlogClient/ITalksCAppService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Z.Ddd.Common.DomainServiceRegister;
+using Z.Ddd.Common.ResultResponse;
+using Z.NetWiki.Application.AlbumsModule.BlogClient.Dto;
+using Z.NetWiki.Application.ArticleModule.BlogClient.Dto;
+using Z.NetWiki.Application.TalksModule.BlogClient.Dto;
+
+namespace Z.NetWiki.Application.TalksModule.BlogClient
+{
+    /// <summary>
+    /// 博客说说
+    /// </summary>
+    public interface ITalksCAppService : IApplicationService
+    {
+        Task<PageResult<TalksOutput>> GetList([FromBody] Pagination dto);
+
+        Task<TalkDetailOutput> TalkDetail([FromQuery] Guid id);
+    }
+}
