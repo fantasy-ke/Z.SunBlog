0103cad49965a185cfa45efac6adde2f13bd2857
Fantasy-Ke <2246080525@qq.com>
2023-10-13 21:50:51 +0800

    说说服务

---
+++ Z.NetWiki.Domain/TalksModule/DomainManager/TagsManager.cs
@@ -0,0 +1,17 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+
+namespace Z.NetWiki.Domain.TalksModule.DomainManager
+{
+    public class TalksManager : BusinessDomainService<Talks>, ITalksManager
+    {
+        public TalksManager(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public override async Task ValidateOnCreateOrUpdate(Talks entity)
+        {
+            await Task.CompletedTask;
+        }
+
+    }
+}
