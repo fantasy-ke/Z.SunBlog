7421c412312050d54cc8a3eddcec14fe8305fdd2
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-13 17:04:59 +0800

    图片manager

---
+++ Z.NetWiki.Domain/PicturesModule/DomainManager/IPicturesManager.cs
@@ -0,0 +1,8 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+
+namespace Z.NetWiki.Domain.PicturesModule.DomainManager
+{
+    public interface IPicturesManager : IBusinessDomainService<Pictures>
+    {
+    }
+}
