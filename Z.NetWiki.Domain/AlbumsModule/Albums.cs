cb473658251a0146c5966bec223554839ea5404a
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-13 17:04:41 +0800

    相册manager

---
+++ Z.NetWiki.Domain/AlbumsModule/Albums.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Z.Ddd.Common.Entities.Auditing;
+using Z.NetWiki.Domain.Enum;
+
+namespace Z.NetWiki.Domain.AlbumsModule;
+
+/// <summary>
+/// 相册表
+/// </summary>
+public class Albums : FullAuditedEntity<Guid>
+{
+    /// <summary>
+    /// 相册名称
+    /// </summary>
+    [MaxLength(32)]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 封面图
+    /// </summary>
+    [MaxLength(256)]
+    public string Cover { get; set; }
+
+    /// <summary>
+    /// 相册类型
+    /// </summary>
+    public CoverType? Type { get; set; }
+
+    /// <summary>
+    /// 可用状态
+    /// </summary>
+    public AvailabilityStatus Status { get; set; }
+
+    /// <summary>
+    /// 排序值（值越小越靠前）
+    /// </summary>
+    public int Sort { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    [MaxLength(256)]
+    public string? Remark { get; set; }
+
+    /// <summary>
+    /// 是否可见
+    /// </summary>
+    public bool IsVisible { get; set; }
+
+    /// <summary>
+    /// 创建人
+    /// </summary>
+    public long CreatedUserId { get; set; }
+
+    /// <summary>
+    /// 标记删除
+    /// </summary>
+    public bool DeleteMark { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    public DateTime CreatedTime { get; set; }
+}
