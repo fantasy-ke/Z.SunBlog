5428c7e589716fa84c015815a89db731ccb01085
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-13 17:04:41 +0800

    相册manager

---
+++ Z.NetWiki.Domain/AlbumsModule/DomainManager/IAlbumsManager.cs
@@ -0,0 +1,8 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+
+namespace Z.NetWiki.Domain.AlbumsModule.DomainManager
+{
+    public interface IAlbumsManager : IBusinessDomainService<Albums>
+    {
+    }
+}
