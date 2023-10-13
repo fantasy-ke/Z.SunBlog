fcf076b33e00520915bafd4eb6457e599fac98e2
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-13 17:05:29 +0800

    命名空间调整

---
+++ Z.NetWiki.Application/ArticleCategoryModule/BlogServer/IArticleCategoryAppService.cs
@@ -0,0 +1,16 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+using Z.NetWiki.Application.ArticleCategoryModule.BlogServer.Dto;
+
+namespace Z.NetWiki.Application.ArticleCategoryModule.BlogServer
+{
+    /// <summary>
+    /// 文章所属栏目管理
+    /// </summary>
+    public interface IArticleCategoryAppService : IApplicationService
+    {
+        Task Create(CreateOrUpdateArticleCategoryDto dto);
+
+
+        Task Update(CreateOrUpdateArticleCategoryDto dto);
+    }
+}
--- Z.NetWiki.Application/NetWikiApplicationModule.cs
+++ Z.NetWiki.Application/NetWikiApplicationModule.cs
@@ -2,7 +2,9 @@
 using Z.Ddd.Common.AutoMapper;
 using Z.Module;
 using Z.Module.Modules;
+using Z.NetWiki.Application.AlbumsModule.BlogServer.MapperConfig;
 using Z.NetWiki.Application.ArticleModule.BlogServer.MapperConfig;
+using Z.NetWiki.Application.TagsModule.BlogServer.MapperConfig;
 using Z.NetWiki.Application.UserModule.MapperConfig;
 using Z.NetWiki.Domain;
 
@@ -29,6 +31,8 @@ namespace Z.NetWiki.Application
                 {
                     UserAutoMapper.CreateMappings(ctx.MapperConfiguration);
                     ArticleSAutoMapper.CreateMappings(ctx.MapperConfiguration);
+                    AlbumsAutoMapper.CreateMappings(ctx.MapperConfiguration);
+                    TagsAutoMapper.CreateMappings(ctx.MapperConfiguration);
                 });
             });
         }
