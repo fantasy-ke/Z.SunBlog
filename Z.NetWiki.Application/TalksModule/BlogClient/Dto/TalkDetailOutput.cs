4870d5c8f2da6ad04c394eaf84ab4d57dde32fbc
Fantasy-Ke <2246080525@qq.com>
2023-10-14 23:59:52 +0800

    Z.SunBlog.Application重命名

--- Z.NetWiki.Application/TalksModule/BlogClient/Dto/TalkDetailOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Z.NetWiki.Application.TalksModule.BlogClient.Dto;
-
-public class TalkDetailOutput : TalksOutput
-{
-    /// <summary>
-    /// 是否允许评论
-    /// </summary>
-    public bool IsAllowComments { get; set; }
-}
