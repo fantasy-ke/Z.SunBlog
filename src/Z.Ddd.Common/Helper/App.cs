430de4e416cd5362168ae9a7235ff9453bc5b465
Fantasy-Ke <2246080525@qq.com>
2023-10-14 11:21:56 +0800

    获取ip地址

--- src/Z.Ddd.Common/Helper/App.cs
+++ src/Z.Ddd.Common/Helper/App.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Z.Ddd.Common.Exceptions;
 
 namespace Z.Ddd.Common;  
 public static class App
@@ -14,12 +16,15 @@ public static class App
     /// </summary>
     public static IServiceProvider RootServices => AppSettings.RootServices;
 
+    
+
+
+
 
     /// <summary>
     /// 获取请求上下文
     /// </summary>
     public static HttpContext HttpContext => CatchOrDefault(() => RootServices.GetRequiredService<IHttpContextAccessor>()?.HttpContext);
-
 
     /// <summary>
     /// 处理获取对象异常问题
@@ -39,5 +44,138 @@ public static class App
         {
             return defaultValue ?? null;
         }
+    }
+
+    /// <summary>
+    /// 获取Ip所属详细地理位置
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public static IpInfoDto GetGeolocation(this HttpContext context)
+    {
+        try
+        {
+            string ip = context.GetRemoteIp();
+            //获取ip信息
+            return GetGeolocation(ip);
+        }
+        catch (Exception e)
+        {
+           throw new UserFriendlyException(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// 获取ip详细信息
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <returns></returns>
+    public static IpInfoDto GetGeolocation(string ip)
+    {
+        if (string.IsNullOrWhiteSpace(ip))
+        {
+            return default;
+        }
+        try
+        {
+            var httpclient = new HttpClient();
+            //获取ip信息
+            string json = httpclient.GetStringAsync($"http://whois.pconline.com.cn/ipJson.jsp?ip={ip}&json=true").GetAwaiter().GetResult();
+            //string json = Encoding.UTF8.GetString(bytes);          
+            return JsonConvert.DeserializeObject<IpInfoDto>(json)?? new IpInfoDto();
+        }
+        catch
+        {
+            return default;
+        }
+    }
+
+    /// <summary>
+    /// 获取ip
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public static string GetRemoteIp(this HttpContext context)
+    {
+        string ip = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+        if (string.IsNullOrWhiteSpace(ip))
+        {
+            ip = context.GetRemoteIpAddressToIPv4();
+        }
+
+        return ip;
+    }
+
+    /// <summary>
+    /// 获取远程 IPv4地址
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public static string GetRemoteIpAddressToIPv4(this HttpContext context)
+    {
+        return context.Connection.RemoteIpAddress?.MapToIPv6()?.ToString();
+    }
+
+    public class IpInfoDto
+    {
+        /// <summary>
+        /// ip
+        /// </summary>
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// 省份
+        /// </summary>
+        [JsonProperty("pro")]
+        public string Province { get; set; }
+
+        /// <summary>
+        /// 省编码
+        /// </summary>
+        [JsonProperty("proCode")]
+        public string ProCode { get; set; }
+
+        /// <summary>
+        /// 城市
+        /// </summary>
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        /// <summary>
+        /// 城市编码
+        /// </summary>
+        [JsonProperty("cityCode")]
+        public string CityCode { get; set; }
+
+        /// <summary>
+        /// 区域
+        /// </summary>
+        [JsonProperty("region")]
+        public string Region { get; set; }
+
+        /// <summary>
+        /// 区编码
+        /// </summary>
+        [JsonProperty("regionCode")]
+        public string RegionCode { get; set; }
+
+        /// <summary>
+        /// IP归属地
+        /// </summary>
+        [JsonProperty("addr")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("regionNames")]
+        public string RegionNames { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        [JsonProperty("err")]
+        public string Error { get; set; }
     }
 }
