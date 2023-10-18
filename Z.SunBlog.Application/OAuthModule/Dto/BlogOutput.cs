087dbd544c34556bf262dceb6df847c4ce49d0e3
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-18 14:55:53 +0800

    第三方登陆服务

---
+++ Z.SunBlog.Application/OAuthModule/Dto/BlogOutput.cs
@@ -0,0 +1,19 @@
+﻿using Z.SunBlog.Core.CustomConfigModule;
+
+namespace Z.SunBlog.Application.OAuthModule.Dto;
+
+public class BlogOutput
+{
+    /// <summary>
+    /// 博客基本设置信息
+    /// </summary>
+    public BlogSetting Site { get; set; }
+    /// <summary>
+    /// 博主信息
+    /// </summary>
+    public BloggerInfo Info { get; set; }
+    /// <summary>
+    /// 各个页面封面图
+    /// </summary>
+    public Dictionary<string, List<string>> Covers { get; set; }
+}
---
+++ Z.SunBlog.Application/OAuthModule/MapperConfig/OAuthAutoMapper.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using AutoMapper;
+using Z.SunBlog.Application.OAuthModule.Dto;
+using Z.SunBlog.Application.TagsModule.BlogServer.Dto;
+using Z.SunBlog.Core.ArticleModule;
+using Z.SunBlog.Core.FriendLinkModule;
+using Z.SunBlog.Core.TagModule;
+
+namespace Z.SunBlog.Application.OAuthModule.BlogServer.MapperConfig
+{
+    public static class OAuthAutoMapper
+    {
+        /// <summary>
+        /// 具体映射规则
+        /// </summary>
+        /// <param name="configuration"></param>
+        public static void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<FriendLink, AddLinkOutput>().ReverseMap();
+            configuration.CreateMap<Article, AddTagInput>().ReverseMap();
+        }
+    }
+}
