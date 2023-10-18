099738177a06026d3149ad0eb8dd3c851a7dc514
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-18 14:56:05 +0800

    个人配置服务

---
+++ Z.SunBlog.Core/CustomConfigModule/DomainManager/ICustomConfigItemManager.cs
@@ -0,0 +1,9 @@
+﻿using Z.Ddd.Common.DomainServiceRegister;
+
+
+namespace Z.SunBlog.Core.CustomConfigModule.DomainManager
+{
+    public interface ICustomConfigItemManager : IBusinessDomainService<CustomConfigItem>
+    {
+    }
+}
