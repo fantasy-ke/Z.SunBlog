b0be821c90b6b8d925efea6d6ae247217f87b656
Fantasy-Ke <2246080525@qq.com>
2023-10-21 17:43:58 +0800

    新增菜单表，新增迁移文件

---
+++ Z.SunBlog.EntityFrameworkCore/Migrations/20231021033251_update_user.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Z.SunBlog.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockExpired",
+                table: "ZUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "ZUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LockExpired",
+                table: "ZUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ZUsers");
+        }
+    }
+}
