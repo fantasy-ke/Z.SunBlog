8140008f50a77416249d0db94a441d4199faaeb1
Fantasy-Ke <2246080525@qq.com>
2023-10-24 20:31:06 +0800

    添加角色菜单的绑定

---
+++ Z.SunBlog.Core/MenuModule/DomainManager/ZRoleMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using Easy.Admin.Core.Entities;
+using Z.Ddd.Common.DomainServiceRegister;
+
+namespace Z.SunBlog.Core.MenuModule.DomainManager
+{
+    public class ZRoleMenuManager : BusinessDomainService<ZRoleMenu>, IZRoleMenuManager
+    {
+        public ZRoleMenuManager(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public override async Task ValidateOnCreateOrUpdate(ZRoleMenu entity)
+        {
+            await Task.CompletedTask;
+        }
+
+    }
+}
