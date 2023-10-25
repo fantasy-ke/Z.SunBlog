614be1673f413f9366ea38550154491b535cfb96
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-25 16:13:47 +0800

    菜单dto

---
+++ Z.SunBlog.Application/MenuModule/Dto/RouterMetaOutput.cs
@@ -0,0 +1,38 @@
+﻿using Z.Ddd.Common.Entities.Enum;
+
+namespace Z.SunBlog.Application.MenuModule.Dto;
+
+public class RouterMetaOutput
+{
+    /// <summary>
+    /// 标题
+    /// </summary>
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// 外链
+    /// </summary>
+    public string? IsLink { get; set; }
+    /// <summary>
+    /// 是否隐藏
+    /// </summary>
+    public bool IsHide { get; set; }
+    /// <summary>
+    /// 是否缓存
+    /// </summary>
+    public bool IsKeepAlive { get; set; }
+    /// <summary>
+    /// 是否固定
+    /// </summary>
+    public bool IsAffix { get; set; }
+
+    /// <summary>
+    /// 菜单
+    /// </summary>
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// 类型
+    /// </summary>
+    public MenuType Type { get; set; }
+}
---
+++ Z.SunBlog.Application/MenuModule/Dto/UpdateSysMenuInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Z.SunBlog.Application.MenuModule.Dto;
+
+public class UpdateSysMenuInput : AddSysMenuInput
+{
+    /// <summary>
+    /// 菜单/按钮Id
+    /// </summary>
+    public Guid Id { get; set; }
+}
