83ef0ee1dd879d1d912a2e4267990d44953f375d
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-25 18:08:22 +0800

    迁移文件调整

---
+++ Z.SunBlog.EntityFrameworkCore/Migrations/20231025095236_update-menu1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Z.SunBlog.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemenu1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedTime",
+                table: "Menu");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedUserId",
+                table: "Menu");
+
+            migrationBuilder.DropColumn(
+                name: "DeleteMark",
+                table: "Menu");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedTime",
+                table: "Menu",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedUserId",
+                table: "Menu",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DeleteMark",
+                table: "Menu",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
