996dfe332561b41e0d10fc4e42a1807be6b9de4b
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-25 16:13:47 +0800

    菜单dto

---
+++ Z.SunBlog.Application/MenuModule/Dto/CheckPermissionOutput.cs
@@ -0,0 +1,13 @@
+﻿namespace Z.SunBlog.Application.MenuModule.Dto;
+
+public class CheckPermissionOutput
+{
+    /// <summary>
+    /// 权限标识
+    /// </summary>
+    public string Code { get; set; }
+    /// <summary>
+    /// 是否有访问权限
+    /// </summary>
+    public bool Access { get; set; }
+}
