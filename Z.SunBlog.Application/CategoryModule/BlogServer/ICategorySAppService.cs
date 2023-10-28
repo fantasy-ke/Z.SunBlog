c803f16d875cbcf49b75da76c7271790a8bdf00c
Fantasy-Ke <2246080525@qq.com>
2023-10-28 16:08:02 +0800

    栏目应用服务

---
+++ Z.SunBlog.Application/CategoryModule/BlogServer/ICategorySAppService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Z.Ddd.Common.DomainServiceRegister;
+using Z.SunBlog.Application.CategoryModule.BlogServer.Dto;
+using Z.SunBlog.Core.SharedDto;
+
+namespace Z.SunBlog.Application.CategoryModule.BlogServer
+{
+    /// <summary>
+    /// 文章栏目管理
+    /// </summary>
+    public interface ICategorySAppService : IApplicationService
+    {
+        Task<List<CategoryPageOutput>> GetPage([FromQuery] string name);
+
+        Task<List<TreeSelectOutput>> TreeSelect();
+
+        Task UpdateCategory(UpdateCategoryInput dto);
+
+        Task AddCategory(AddCategoryInput dto);
+
+        Task Delete(KeyDto dto);
+
+    }
+}
---
+++ Z.SunBlog.Application/CategoryModule/BlogServer/MapperConfig/CategoryAutoMapper.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using AutoMapper;
+using Z.SunBlog.Application.CategoryModule.BlogServer.Dto;
+using Z.SunBlog.Application.PictureModule.BlogServer.Dto;
+using Z.SunBlog.Core.CategoriesModule;
+using Z.SunBlog.Core.PicturesModule;
+using Z.SunBlog.Core.SharedDto;
+
+namespace Z.SunBlog.Application.CategoryModule.BlogServer.MapperConfig
+{
+    public static class CategoryAutoMapper
+    {
+        /// <summary>
+        /// 具体映射规则
+        /// </summary>
+        /// <param name="configuration"></param>
+        public static void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<Categories, AddCategoryInput>().ReverseMap();
+            configuration.CreateMap<Categories, UpdateCategoryInput>().ReverseMap();
+            configuration.CreateMap<Categories, CategoryPageOutput>().ReverseMap();
+            configuration.CreateMap<Categories, TreeSelectOutput>()
+                .ForMember(dest => dest.Value, src => src.MapFrom(c => c.Id))
+                .ForMember(dest => dest.Label, src => src.MapFrom(c => c.Name));
+        }
+    }
+}
