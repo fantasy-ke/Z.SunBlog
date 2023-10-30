beb435bcdc0bef11ce13fc7f36041ace50bf0cf0
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-30 18:06:26 +0800

    minio文件上传

---
+++ Z.SunBlog.Application/FileModule/Dto/UploadFileOutput.cs
@@ -0,0 +1,14 @@
+﻿namespace Z.SunBlog.Application.FileModule.Dto;
+
+public class UploadFileOutput
+{
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 附件链接
+    /// </summary>
+    public string Url { get; set; }
+}
---
+++ Z.SunBlog.Core/MinioFileModule/DomainManager/IMinioFileManager.cs
@@ -0,0 +1,26 @@
+﻿using Z.Ddd.Common.DomainServiceRegister.Domain;
+using Z.Ddd.Common.Minio;
+using Z.Module.DependencyInjection;
+
+namespace Z.SunBlog.Core.MinioFileModule.DomainManager
+{
+    public interface IMinioFileManager : IDomainService, ITransientDependency
+    {
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="file"></param>
+        /// <param name="contentType"></param>
+        /// <returns></returns>
+        Task UploadMinio(Stream stream, string file, string contentType);
+
+
+        /// <summary>
+        /// 获取文件
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        Task<ObjectOutPut> GetFile(string filename);
+    }
+}
