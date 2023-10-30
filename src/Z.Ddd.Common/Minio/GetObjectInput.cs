f61fcc84e5583378905227437e50cb12c7c3bf7e
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-30 16:33:32 +0800

    minio配置修改

--- src/Z.Ddd.Common/Minio/GetObjectInput.cs
+++ src/Z.Ddd.Common/Minio/GetObjectInput.cs
@@ -2,7 +2,13 @@
 
 public class GetObjectInput
 {
+    /// <summary>
+    /// 命名空间
+    /// </summary>
     public string? BucketName { get; set; }  
 
+    /// <summary>
+    /// 对象名称
+    /// </summary>
     public string? ObjectName { get; set; }
 }
--- src/Z.Ddd.Common/Minio/RemoveObjectInput.cs
+++ src/Z.Ddd.Common/Minio/RemoveObjectInput.cs
@@ -5,9 +5,9 @@ public class RemoveObjectInput
     /// <summary>
     /// 存储桶名称
     /// </summary>
-    public string BucketName { get; set; }
+    public string? BucketName { get; set; }
     /// <summary>
     /// 存储对象名称
     /// </summary>
-    public string ObjectName { get; set; }
+    public string? ObjectName { get; set; }
 }
