ef010d7f0019518cfda61378eeebe9d7c75317ba
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-11-06 15:04:21 +0800

    配置dto修改必填项

--- Z.SunBlog.Application/ConfigModule/Dto/CustomConfigSetJsonInput.cs
+++ Z.SunBlog.Application/ConfigModule/Dto/CustomConfigSetJsonInput.cs
@@ -9,11 +9,11 @@ public class CustomConfigSetJsonInput
     /// 自定义配置ID
     /// </summary>
     [Required(ErrorMessage = "缺少必要参数")]
-    public long Id { get; set; }
+    public Guid Id { get; set; }
 
     /// <summary>
     /// 表单设计
     /// </summary>
     [Required(ErrorMessage = "请设计表单")]
-    public JObject Json { get; set; }
+    public dynamic Json { get; set; }
 }
---
+++ Z.SunBlog.Application/ConfigModule/Dto/GetConfigDetailInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.SunBlog.Application.ConfigModule.Dto
+{
+    public class GetConfigDetailInput
+    {
+        public Guid Id { get; set; }
+
+        public Guid? ItemId { get; set; }
+    }
+}
