424e0a30ce9dbed4ef42741c11c7d9bbb5597f49
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-12-01 17:11:05 +0800

    添加mysq链接字符串

--- Z.SunBlog.EntityFrameworkCore/SunBlogEntityFrameworkCoreModule.cs
+++ Z.SunBlog.EntityFrameworkCore/SunBlogEntityFrameworkCoreModule.cs
@@ -3,9 +3,13 @@ using Z.EntityFrameworkCore;
 using Z.EntityFrameworkCore.Core;
 using Z.EntityFrameworkCore.SqlServer;
 using Z.EntityFrameworkCore.SqlServer.Extensions;
+using Z.EntityFrameworkCore.Mysql.Extensions;
 using Z.Module;
+using Z.Module.Extensions;
 using Z.Module.Modules;
 using Z.SunBlog.EntityFrameworkCore.EntityFrameworkCore.Seed;
+using Microsoft.Extensions.Configuration;
+using Z.Ddd.Common.Exceptions;
 
 namespace Z.SunBlog.EntityFrameworkCore
 {
@@ -14,7 +18,22 @@ namespace Z.SunBlog.EntityFrameworkCore
     {
         public override void ConfigureServices(ServiceConfigerContext context)
         {
-            context.AddSqlServerEfCoreEntityFrameworkCore<SunBlogDbContext>();
+            var configuration = context.GetConfiguration();
+            var dbtype = configuration.GetSection("App:DbType").Get<string>()!;
+            string? connectionString = string.Empty;
+            switch (dbtype.ToLower())
+            {
+                case "sqlserver":
+                    connectionString = configuration.GetSection("App:ConnectionString:SqlServer").Get<string>();
+                    context.AddSqlServerEfCoreEntityFrameworkCore<SunBlogDbContext>(connectionString);
+                    break;
+                case "mysql":
+                    connectionString = configuration.GetSection("App:ConnectionString:Mysql").Get<string>();
+                    context.AddMysqlEfCoreEntityFrameworkCore<SunBlogDbContext>(new Version(5, 7), connectionString);
+                    break;
+                default:
+                    throw new UserFriendlyException("不支持的数据库类型");
+            }
             context.UseRepository<SunBlogDbContext>();
         }
 
