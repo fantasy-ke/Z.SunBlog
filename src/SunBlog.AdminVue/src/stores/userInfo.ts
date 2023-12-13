2b35485b996841d4e1e114fc0458b237c36d4d2d
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-12-13 12:44:44 +0800

    异步查询

--- src/SunBlog.AdminVue/src/stores/userInfo.ts
+++ src/SunBlog.AdminVue/src/stores/userInfo.ts
@@ -2,43 +2,46 @@ import { defineStore } from 'pinia';
 // import Cookies from 'js-cookie';
 // import { Session } from '@/utils/storage';
 import { computed, inject } from 'vue';
-import { UsersServiceProxy, ZFantasyToken } from '@/shared/service-proxies';
+import { AuthsServiceProxy, UsersServiceProxy, ZFantasyToken } from '@/shared/service-proxies';
 import apiHttpClient from '@/utils/http';
 import { StoreKey, useUserStore } from './user';
 const _usersCService = new UsersServiceProxy(inject('$baseurl'), apiHttpClient as any);
+const _authService = new AuthsServiceProxy(inject('$baseurl'), apiHttpClient as any);
 
 /**
  * 用户信息
  * @methods setUserInfos 设置用户信息
  */
 export const useUserInfo = defineStore(StoreKey.UserInfo, () => {
-	const userStore = useUserStore();
-	const userInfoState = computed(() => userStore);
+  const userStore = useUserStore();
+  const userInfoState = computed(() => userStore);
 
-	//用户信息
-	const userInfo = computed(() => userStore.userInfo);
+  //用户信息
+  const userInfo = computed(() => userStore.userInfo);
 
-	const setToken = (zToken: ZFantasyToken) => {
-		userStore.setToken({ zToken: zToken });
-	};
+  const setToken = (zToken: ZFantasyToken) => {
+    userStore.setToken({ zToken: zToken });
+  };
 
-	const clearToken = () => {
-		userStore.$reset();
-	};
-	/**
-	 * 获取当前用户基本信息
-	 */
-	const getUserInfo = () => {
-		_usersCService.currentUserInfo().then(res=>{
-        userStore.setUserInfo(res.result!);
-     }).catch(() => {
-      clearToken();
-    })
-		
-	};
-	/**
-	 * 获取当前用户基本信息
-	 */
+  const clearToken = async () => {
+    await _authService.zSignOut(userStore?.zToken?.accessToken);
+    userStore.$reset();
 
-	return { userInfoState, userInfo, getUserInfo, setToken, clearToken };
+  };
+  /**
+   * 获取当前用户基本信息
+   */
+  const getUserInfo = async () => {
+    const { result } = await _usersCService.currentUserInfo();
+    if (result.userName) {
+      userStore.setUserInfo(result!);
+      return;
+    }
+    await clearToken();
+  };
+  /**
+   * 获取当前用户基本信息
+   */
+
+  return { userInfoState, userInfo, getUserInfo, setToken, clearToken };
 });
