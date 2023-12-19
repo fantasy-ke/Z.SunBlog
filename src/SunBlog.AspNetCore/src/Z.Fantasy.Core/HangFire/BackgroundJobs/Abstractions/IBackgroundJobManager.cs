0831282e33d598960de92a78f2a96f7eb5675644
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-12-19 15:38:45 +0800

    删除队列优先🚋

--- src/SunBlog.AspNetCore/src/Z.Fantasy.Core/HangFire/BackgroundJobs/Abstractions/IBackgroundJobManager.cs
+++ src/SunBlog.AspNetCore/src/Z.Fantasy.Core/HangFire/BackgroundJobs/Abstractions/IBackgroundJobManager.cs
@@ -14,11 +14,9 @@ public interface IBackgroundJobManager
     /// <typeparam name="TArgs">参数Jobs.</typeparam>
     /// <param name="args">参数Jobs</param>
     ///<param name="delay">任务延期时间，默认null，队列直接执行，给值就延迟执行</param>
-    /// <param name="priority">优先级，暂时没用.</param>
 
     Task<string> EnqueueAsync<TArgs>(
         TArgs args,
-        TimeSpan? delay = null,
-        BackgroundJobPriority priority = BackgroundJobPriority.Normal
+        TimeSpan? delay = null
     );
 }
