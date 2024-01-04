8177b9b2208a8201bf8261557b11bbe819d463db
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2024-01-04 18:24:00 +0800

    迁移文件

---
+++ src/SunBlog.AspNetCore/Z.SunBlog.EntityFrameworkCore/Migrations/20240104100714_update-file.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Z.SunBlog.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatefile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FileType",
+                table: "ZFileInfo",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileTypeString",
+                table: "ZFileInfo",
+                type: "longtext",
+                nullable: true,
+                collation: "utf8mb4_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileType",
+                table: "ZFileInfo");
+
+            migrationBuilder.DropColumn(
+                name: "FileTypeString",
+                table: "ZFileInfo");
+        }
+    }
+}
