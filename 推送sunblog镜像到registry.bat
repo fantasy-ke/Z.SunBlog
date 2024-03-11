32c94e7ffefb8be4013b662d1bed77d0eaa1d784
fantasy_ke <2246080525@qq.com>
2024-03-11 21:45:58 +0800

    脚本修改

--- 推送sunblog镜像到registry.bat
+++ 推送sunblog镜像到registry.bat
@@ -1,7 +1,9 @@
+chcp 65001
 @echo off
 cd /d %~dp0
 
 echo "push images start"
+echo "安装PowerShell7！！！！！！"
 
 pwsh .\push_sunblog_imgs.ps1
 
--- 编译sunblog镜像.bat
+++ 编译sunblog镜像.bat
@@ -1,7 +1,9 @@
+chcp 65001
 @echo off
 cd /d %~dp0
 
 echo "build images start"
+echo "安装PowerShell7！！！！！！"
 
 pwsh .\build_sunblog_imgs.ps1
 
