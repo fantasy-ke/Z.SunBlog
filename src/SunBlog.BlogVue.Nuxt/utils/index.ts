0003a46491e511007a02d36b5dd04b1f4bf6a71b
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2024-04-22 12:37:00 +0800

    全局时间转换格式方法🥵🪔

--- src/SunBlog.BlogVue.Nuxt/utils/index.ts
+++ src/SunBlog.BlogVue.Nuxt/utils/index.ts
@@ -1,3 +1,6 @@
+import type { Dayjs } from "dayjs";
+import moment from "moment";
+
 /**
  * 随机整数
  * @param min 最小值
@@ -12,7 +15,7 @@ export const randomNumber = (min: number, max: number) => {
    * 生成uuid
    * @returns 
    */
-  export const generateUUID = () => {
+export const generateUUID = () => {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
       const r = (Math.random() * 16) | 0,
         v = c === "x" ? r : (r & 0x3) | 0x8;
@@ -20,3 +23,6 @@ export const randomNumber = (min: number, max: number) => {
     });
   };
   
+export const formatDate = (date: string | number | moment.Moment | Date  | null | undefined, format: string = "YYYY-MM-DD HH:mm:ss"): string => {
+    return moment(date).format(format)
+};
